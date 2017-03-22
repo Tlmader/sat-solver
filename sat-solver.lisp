d21f2d0b785de0b7972dd4ca7d31e68fb543abd1
Ted Mader <tlmader.dev@gmail.com>
2017-03-21 22:50:10 -0500

    Reformat code and disable 1-13

--- sat-solver.lisp
+++ sat-solver.lisp
@@ -37,17 +37,18 @@
  (let ((simpl (simplify cnf))
        (result nil))
   (cond ((eq simpl t) t) ; If every clause is true, return true.
-   ((eq simpl nil) nil)    ; If any clause is empty, return false.
+   ((eq simpl nil) nil)  ; If any clause is empty, return false.
    ((symbolp simpl) t)
-   ((mapcar (lambda (var)
-  ; Set V=T. Try to find a model that satisfies.
-             (cond ((eq (satisfiable(set-variable simpl var t)) t)
-                    (setq result t))
-  ; Set V=F. Try to find a model that satisfies.
-              ((eq (satisfiable(set-variable simpl var nil)) t)
-               (setq result t))))
+   ((mapcar
+     (lambda (var)
+       ; Set V=T. Try to find a model that satisfies.
+      (cond ((eq (satisfiable(set-variable simpl var t)) t)
+             (setq result t))
+       ; Set V=F. Try to find a model that satisfies.
+       ((eq (satisfiable(set-variable simpl var nil)) t)
+        (setq result t))))
      (remove 'and (remove 'or (remove 'not (remove-duplicates (flatten simpl))))))
-    result)))) ; Return false.
+    result)))) ; Return result.
 
 ;;; Feel free to define other functions here if you want.
 ;;; Simplifies a CNF formula
@@ -80,13 +81,15 @@
     (set-variable part variable value))
    cnf)))
 
+;;; Flattens a list.
 (defun flatten (orig-lst)
- (if (eql orig-lst nil)
+ (if (eq orig-lst nil)
   nil
-  (let ((elem (car orig-lst)) (resto-lst (cdr orig-lst)))
+  (let ((elem (car orig-lst))
+        (rest-of-lst (cdr orig-lst)))
    (if (listp elem)
-    (append (flatten elem) (flatten resto-lst))
-    (append (cons elem nil) (flatten resto-lst))))))
+    (append (flatten elem) (flatten rest-of-lst))
+    (append (cons elem nil) (flatten rest-of-lst))))))
 
 ;;; ---------- END STUDENT CODE ----------
 
@@ -98,17 +101,19 @@
 ;;; The format of a test is: (number expression expected-value).
 ;;; For example, expression #6 is (and a b) and it is satisfiable, so the
 ;;; expected return value of #'satisfiable is t.
-(mapcar #'grade '((1 t t)
-                  (2 nil nil)
-                  (3 a t)
-                  (4 (and t) t)
-                  (5 (and nil) nil)
-                  (6 (and a b) t)
-                  (7 (or t) t)
-                  (8 (or nil) nil)
-                  (9 (or a b) t)
-                  (10 (and a (not a)) nil)
-                  (11 (and a (or (not a) b)) t)
-                  (12 (and a b (or (not a) (not b))) nil)
-                  (13 (and (or a b c) (not b) (or (not a) c) (or a d) (not c)) nil)))
-                  ; (14 (and (not a) (not b) (not c) (not d) (not e) (not f) (not g) (not h) (not i) (not j) (or a b c d e f g h i j k)) t)))
+; (mapcar #'grade '((1 t t)
+;                   (2 nil nil)
+;                   (3 a t)
+;                   (4 (and t) t)
+;                   (5 (and nil) nil)
+;                   (6 (and a b) t)
+;                   (7 (or t) t)
+;                   (8 (or nil) nil)
+;                   (9 (or a b) t)
+;                   (10 (and a (not a)) nil)
+;                   (11 (and a (or (not a) b)) t)
+;                   (12 (and a b (or (not a) (not b))) nil)
+;                   (13 (and (or a b c) (not b) (or (not a) c) (or a d) (not c)) nil)
+;                   (14 (and (not a) (not b) (not c) (not d) (not e) (not f) (not g) (not h) (not i) (not j) (or a b c d e f g h i j k)) t)))
+
+(mapcar #'grade '((14 (and (not a) (not b) (not c) (not d) (not e) (not f) (not g) (not h) (not i) (not j) (or a b c d e f g h i j k)) t)))
