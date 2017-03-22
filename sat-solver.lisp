971a389d3ef02c8cd553821422d801e1ce9431ec
Ted Mader <tlmader.dev@gmail.com>
2017-03-21 22:45:13 -0500

    Reformat code

--- sat-solver.lisp
+++ sat-solver.lisp
@@ -5,25 +5,25 @@
 (defvar *max-calls* 10)
 
 (defmacro defun-limited (name args &body body)
-  "Defines a function which can only be called *max-calls* number of times."
-  `(defun ,name ,args
-     (let ((result nil))
-       (progn (setq *calls* (1+ *calls*))
-              (when (> *calls* *max-calls*)
-                (error "Call limit exceeded."))
-              (setq result ,@body)
-              (setq *calls* (1- *calls*))
-              result))))
+ "Defines a function which can only be called *max-calls* number of times."
+ `(defun ,name ,args
+   (let ((result nil))
+    (progn (setq *calls* (1+ *calls*))
+     (when (> *calls* *max-calls*)
+      (error "Call limit exceeded."))
+     (setq result ,@body)
+     (setq *calls* (1- *calls*))
+     result))))
 
 (defun grade (problem)
-  "A function for grading an individual problem instance."
-  (let ((number (first problem))
-        (expression (second problem))
-        (correct-answer (third problem))
-        (your-answer 'error))
-    (unwind-protect
-        (setq your-answer (satisfiable (second problem)))
-      (format t "Expression ~S: ~S~%  Correct Answer: ~S~%  Your Answer:    ~S~%" number expression correct-answer your-answer))))
+ "A function for grading an individual problem instance."
+ (let ((number (first problem))
+       (expression (second problem))
+       (correct-answer (third problem))
+       (your-answer 'error))
+  (unwind-protect
+   (setq your-answer (satisfiable (second problem)))
+   (format t "Expression ~S: ~S~%  Correct Answer: ~S~%  Your Answer:    ~S~%" number expression correct-answer your-answer))))
 
 ;;; ---------- BEGIN STUDENT CODE ----------
 ;;; Your code will be pasted in starting here.
@@ -40,53 +40,53 @@
    ((eq simpl nil) nil)    ; If any clause is empty, return false.
    ((symbolp simpl) t)
    ((mapcar (lambda (var)
-          ; Set V=T. Try to find a model that satisfies.
-          (cond ((eq (satisfiable(set-variable simpl var t)) t)
-                 (setq result t))
-          ; Set V=F. Try to find a model that satisfies.
-                ((eq (satisfiable(set-variable simpl var nil)) t)
-                 (setq result t))))
+  ; Set V=T. Try to find a model that satisfies.
+             (cond ((eq (satisfiable(set-variable simpl var t)) t)
+                    (setq result t))
+  ; Set V=F. Try to find a model that satisfies.
+              ((eq (satisfiable(set-variable simpl var nil)) t)
+               (setq result t))))
      (remove 'and (remove 'or (remove 'not (remove-duplicates (flatten simpl))))))
     result)))) ; Return false.
 
 ;;; Feel free to define other functions here if you want.
 ;;; Simplifies a CNF formula
 (defun simplify (cnf)
-  (if (symbolp cnf)
-   cnf
-   (let ((simplified (mapcar #'simplify cnf)))
-    (cond ((eq (first simplified) 'not)
-           (cond ((eq (second simplified) t) nil)
-            ((eq (second simplified) nil) t)
-            (t simplified)))
-     ((eq (first simplified) 'and)
-      (let ((shorter (remove t simplified)))
-       (cond ((equal shorter '(and)) t)
-        ((member nil shorter) nil)
-        (t shorter))))
-     ((eq (first simplified) 'or)
-      (let ((shorter (remove nil simplified)))
-       (cond ((equal shorter '(or)) nil)
-        ((member t shorter) t)
-        ((eq 2 (length shorter)) (second shorter))
-        (t shorter))))))))
+ (if (symbolp cnf)
+  cnf
+  (let ((simplified (mapcar #'simplify cnf)))
+   (cond ((eq (first simplified) 'not)
+          (cond ((eq (second simplified) t) nil)
+           ((eq (second simplified) nil) t)
+           (t simplified)))
+    ((eq (first simplified) 'and)
+     (let ((shorter (remove t simplified)))
+      (cond ((equal shorter '(and)) t)
+       ((member nil shorter) nil)
+       (t shorter))))
+    ((eq (first simplified) 'or)
+     (let ((shorter (remove nil simplified)))
+      (cond ((equal shorter '(or)) nil)
+       ((member t shorter) t)
+       ((eq 2 (length shorter)) (second shorter))
+       (t shorter))))))))
 
 ;;; Substitutes a value in a CNF formula
 (defun set-variable (cnf variable value)
-  (if (symbolp cnf)
-    (if (eq cnf variable) value cnf)
-    (mapcar
-      (lambda (part)
-        (set-variable part variable value))
-     cnf)))
+ (if (symbolp cnf)
+  (if (eq cnf variable) value cnf)
+  (mapcar
+   (lambda (part)
+    (set-variable part variable value))
+   cnf)))
 
 (defun flatten (orig-lst)
-   (if (eql orig-lst nil)
-       nil
-       (let ((elem (car orig-lst)) (resto-lst (cdr orig-lst)))
-           (if (listp elem)
-               (append (flatten elem) (flatten resto-lst))
-               (append (cons elem nil) (flatten resto-lst))))))
+ (if (eql orig-lst nil)
+  nil
+  (let ((elem (car orig-lst)) (resto-lst (cdr orig-lst)))
+   (if (listp elem)
+    (append (flatten elem) (flatten resto-lst))
+    (append (cons elem nil) (flatten resto-lst))))))
 
 ;;; ---------- END STUDENT CODE ----------
 
